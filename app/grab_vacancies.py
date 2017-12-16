eeca099570843340def581d9ea955dabf88901e4
fakedup <sokolov.nikita@gmail.com>
2017-12-16 13:03:10 +0300

    threading on vacancies download w some integrity errors

--- app/grab_vacancies.py
+++ app/grab_vacancies.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime
 from db import db_session, Vacancy, Area, Industry
 import sqlalchemy.exc
+import threading
 
 base_url = 'https://api.hh.ru/vacancies/'
 
@@ -35,42 +36,75 @@ headers = {
 def get_vacancy(id):
     null = None
     request = requests.get(base_url+str(id), headers = headers)
-    vacancy = json.loads(request.text)
-    name = vacancy['name']
-    area = int(vacancy['area']['id'])
-    description = vacancy['description']
+    print (request.status_code)
+    if request.status_code == 200:
 
-    if vacancy['salary']:
-        salary_from = int(vacancy['salary'].get('from'))
-        salary_to = int(vacancy['salary'].get('to'))
-        salary_gross = vacancy['salary'].get('gross')
-        if salary_gross == 'true':
-            salary_gross = True
-        elif salary_gross == 'false':
-            salary_gross = False
+        vacancy = json.loads(request.text)
+        name = vacancy['name']
+        print (name)
+        area = int(vacancy['area']['id'])
+        description = vacancy['description']
+
+        if vacancy['salary']:
+            salary_from = int(vacancy['salary'].get('from'))
+            salary_to = int(vacancy['salary'].get('to'))
+            salary_gross = vacancy['salary'].get('gross')
+            if salary_gross == 'true':
+                salary_gross = True
+            elif salary_gross == 'false':
+                salary_gross = False
+        else:
+            salary_from = null
+            salary_to = null
+            salary_gross = null
+
+        published_at = datetime.strptime(vacancy['published_at'][:-5], '%Y-%m-%dT%H:%M:%S')
+
+        exp = vacancy['experience']['name']
+        employment = vacancy['employment']['name']
+        employer = vacancy['employer']['name']
+
+
+        return (name, area, description, 
+                salary_from, salary_to, salary_gross, 
+                published_at, exp, employment , employer)
+
     else:
-        salary_from = null
-        salary_to = null
-        salary_gross = null
 
-    published_at = datetime.strptime(vacancy['published_at'][:-5], '%Y-%m-%dT%H:%M:%S')
-
-    exp = vacancy['experience']['name']
-    employment = vacancy['employment']['name']
-    employer = vacancy['employer']['name']
-
-
-    return (name, area, description, 
-            salary_from, salary_to, salary_gross, 
-            published_at, exp, employment , employer)
+        return None
 
 def put_vacancy(id):
-    if not Vacancy.query.filter(Vacancy.id == id).first():
+    # if not Vacancy.query.filter(Vacancy.id == id).first():
+    try:
         name, area, description, salary_from, salary_to, salary_gross, published_at, exp, employment , employer = get_vacancy(id)
+
+        print (name)
+    
         vacancy_to_add = Vacancy(id, name, area, description, salary_from, salary_to, salary_gross, published_at, exp, employment , employer)
+
         db_session.add(vacancy_to_add)
+    except (TypeError, sqlalchemy.exc.IntegrityError, sqlite3.IntegrityError):
+        print ('INTEGRITY ERROR')
+
+def put_vacancies_range(start_id, number_to_put=1000):
+    for vacancy_id in range(start_id, start_id+number_to_put):
+        put_vacancy(vacancy_id)
+    db_session.commit()
+
+def put_vacancies_range_threads(start_id, number_to_put=1000, threads_number=4):
+    threads = []
+    for i in range(threads_number):
+        print (i)
+        one_thread = threading.Thread(target = put_vacancies_range, args = (start_id + i * number_to_put, number_to_put ))
+        threads.append(one_thread)
+
+    for i in range(threads_number):
+        threads[i].start()
+
+    for i in range(threads_number):
+        threads[i].join()
+
+
 
 if __name__ == '__main__':
-    id = 10000001
-    put_vacancy (id)
-    db_session.commit()
+    put_vacancies_range_threads (11000008, 1, 4)
